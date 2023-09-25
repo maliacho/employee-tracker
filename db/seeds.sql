640a99212375eed864f28132fd50f9dad7e722aa
maliacho <maliacho21@gmail.com>
2023-09-25 16:40:05 -0700

    filled out schema and seeds file. began to work on index.js

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO department (name)
+VALUES ('Finance'),
+('Human Resources'),
+('Administration'),
+('Marketing'),
+('Sales'),
+('IT'),
+('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Accountant', 80000, 1),
+('HR Representative', 75000, 2),
+('Front Desk', 70000, 3),
+('Social Media Strategist', 80000, 4),
+('Sales Representative', 75000, 5),
+('Cyber Security', 75000, 6),
+('Attorney', 80000, 7);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Malia', 'Cho', 1, NULL),
+('Chan', 'Nguyen', 2, 1),
+('Allie', 'Deaver', 3, 1),
+('Vanna', 'Luciano', 4, 1),
+('Linda', 'Vuong', 5, NULL),
+('Nikki', 'Campagna', 6, NULL),
+('Megan', 'Umali', 7, NULL);
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2')
 
 
-const questions : [
+const db = mysql.createConnection(
     {
-        type:
-        
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'company_db'
     },
-];
+    console.log('Connected to the company database.')
+);
+
+const questions = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'choiceInput',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update an Employee Role'
+        ]}
+]
+
+const handlerFunction = (err, res) => {
+    if (res === 'View All Departments') {
+        db.query('SELECT * FROM department', (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.table(result)
+        })
+    }
+};
+
+const init = (questionsArray, handlerFunction) => {
+    inquirer
+        .prompt(questionsArray)
+        .then(handlerFunction)
+};
+
+init(questions, handlerFunction);
